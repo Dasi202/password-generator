0ca2a85fd8a0052518451584a700290eaae52daa
Danlon Situma <situmadanlon@gmail.com>
2021-04-12 04:29:07 +0300

    Add the credential class

---
+++ credential.py
@@ -0,0 +1,12 @@
+class Credential:
+    """
+    Class that generates new credentials for  users
+    """
+
+    credential_list = []
+
+    def __init__(self, title, url, user_name, password):
+        self.title = title
+        self.url = url
+        self.user_name = user_name
+        self.password = password
