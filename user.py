ecdd22b75066859d60c0da17792275eea3c791f5
Danlon Situma <situmadanlon@gmail.com>
2021-04-12 06:20:45 +0300

    Add save user method

--- user.py
+++ user.py
@@ -1,12 +1,17 @@
 class User:
     """
-    Class that generates new instances of user.
+    Class that generates new instances of users
     """
 
     user_list = []
 
-    def _init_(self,first_name,last_name,user_name,password):
+    def __init__(self, first_name, last_name, user_name, password):
         self.first_name = first_name
-        self.last_name  = last_name
+        self. last_name = last_name
         self.user_name = user_name
         self.password = password
+
+    def save_user(self):
+        User.user_list.append(self)
+
+   
