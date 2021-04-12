b6a9077214b8eabd290c92c1182687b54590ba48
Danlon Situma <situmadanlon@gmail.com>
2021-04-12 07:23:59 +0300

    Add delete credential test

--- credential_test.py
+++ credential_test.py
@@ -29,6 +29,15 @@ class TestCredential(unittest.TestCase):
         test_credential.save_credential()
         self.assertEqual(len(Credential.credential_list), 2)
 
+    def test_delete_credential(self):
+        self.new_credential.save_credential()
+        test_credential = Credential("Gmail", "https://gmail.com/signup/", "situwali", "google")
+        test_credential.save_credential()
+
+        self.new_credential.delete_credential()
+        self.assertEqual(len(Credential.credential_list), 1)
+
+
 if __name__ == '__main__':
     unittest.main()
 
