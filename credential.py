c1633e874c463223035343839552373b2328ae84
Danlon Situma <situmadanlon@gmail.com>
2021-04-12 07:13:41 +0300

    Add save credential method

--- credential.py
+++ credential.py
@@ -1,8 +1,7 @@
 class Credential:
     """
-    Class that generates new credentials for  users
+    Class that generates new credentials for users
     """
-
     credential_list = []
 
     def __init__(self, title, url, user_name, password):
@@ -10,3 +9,6 @@ class Credential:
         self.url = url
         self.user_name = user_name
         self.password = password
+
+    def save_credential(self):
+        Credential.credential_list.append(self)
